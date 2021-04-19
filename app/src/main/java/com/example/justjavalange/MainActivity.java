63045d4568f1ee978d47abdf2f21686e1b078001
Daniel Clark <02daniel.clark@gulllakecs.org>
2021-04-19 10:32:28 -0400

    4.6 - Added the pricePerCup @param to the calculatePrice method

--- app/src/main/java/com/example/justjavalange/MainActivity.java
+++ app/src/main/java/com/example/justjavalange/MainActivity.java
@@ -1,20 +1,25 @@
+/**
+ * The package in this file has to match the AndroidManifest.xml package found in the manifests
+ * folder and if this files package is different then change this files package not the
+ * AndroidManifest package
+ */
 
 package com.example.justjavalange;
 
-
-import android.os.Bundle;
-
-import androidx.appcompat.app.AppCompatActivity;
-
-import android.view.View;
-import android.widget.TextView;
-
 import java.text.NumberFormat;
 
-
+import android.os.Bundle;
+import androidx.appcompat.app.AppCompatActivity;
+import android.view.View;
+import android.widget.TextView;
+/**
+ * This app displays an order form to order coffee.
+ */
 public class MainActivity extends AppCompatActivity {
 
-
+    /**
+     * Setting Global Quantity
+     */
 
     int quantity = 0;
 
@@ -24,27 +29,58 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
     }
 
+    /**
+     * This method has the quantity increase
+     * @param view
+     */
 
     public void increment(View view) {
         quantity++;
-        displayQuantity(quantity);
+        display(quantity);
     }
 
+    /**
+     * This method allows for the quantity to be decreased but limits it to having to be greater or equal to 0 as its lowest quantity
+     * @param view
+     */
 
-    public void decrement(View view)  {
-        if (quantity>0) {
+    public void decrement(View view){
+        if(quantity>0) {
             quantity--;
-            displayQuantity(quantity);
+            display(quantity);
         }
     }
 
 
+    /**
+     * This method is called when the order button is clicked.
+     */
     public void submitOrder(View view) {
+        // display(quantity);
+        // displayPrice(coffeesOrdered * 5);
         // int coffeesOrdered = quantity;
         int price = quantity * 5;
         String priceMessage = "Thank you for ordering " + quantity + " Coffees! \nAmount Due: $" + price; //I used an escape sequence \" to include the quotes around free
         priceMessage = priceMessage + "\n\nYour order will be right up!"; //Double \n escape key for w line separation
         displayMessage(priceMessage);
+    }
+
+
+
+    /**
+     * This method displays the given quantity value on the screen.
+     */
+    private void display(int number) {
+        TextView quantityTextView = (TextView) findViewById(R.id.quantity_text_view);
+        quantityTextView.setText("" + number);
+    }
+
+    /**
+     * This method displays the given price on the screen.
+     */
+    private void displayPrice(int number) {
+        TextView priceTextView = (TextView) findViewById(R.id.price_text_view);
+        priceTextView.setText(NumberFormat.getCurrencyInstance().format(number));
     }
     /**
      * This method displays the given text on the screen.
@@ -53,16 +89,7 @@ public class MainActivity extends AppCompatActivity {
     private void displayMessage(String message) {
         TextView priceTextView = (TextView) findViewById(R.id.price_text_view);
         priceTextView.setText(message);
-    }
 
-    private void displayQuantity(int number) {
-        TextView quantityTextView = (TextView) findViewById(R.id.quantity_text_view);
-        quantityTextView.setText(" " + number);
-
-    }
-    private void displayPrice ( int number){
-        TextView priceTextView = (TextView) findViewById(R.id.price_text_view);
-        priceTextView.setText(NumberFormat.getCurrencyInstance().format(number));
     }
 
 }
