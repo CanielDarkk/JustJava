ec7229df2b2271cae282e0a15b22cff201328c68
Daniel Clark <02daniel.clark@gulllakecs.org>
2021-05-13 18:39:03 -0400

    Added name Parameter and Fixed Code.

--- app/src/main/java/com/example/justjavalange/MainActivity.java
+++ app/src/main/java/com/example/justjavalange/MainActivity.java
@@ -2,8 +2,8 @@
 package com.example.justjavalange;
 
 
-import android.os.Bundle;
 
+import android.os.Bundle;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.util.Log;
@@ -11,14 +11,16 @@ import android.view.View;
 import android.widget.CheckBox;
 import android.widget.EditText;
 import android.widget.TextView;
-
 import java.text.NumberFormat;
 
-
+/**
+ * This app displays an order form to order coffee.
+ */
 public class MainActivity extends AppCompatActivity {
 
-
-
+    /**
+     *Declare Global Variables Here
+     */
     int quantity = 0;
 
     @Override
@@ -27,75 +29,118 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
     }
 
-
+    /**
+     * This method is called when the Increment/Plus button is clicked.
+     */
     public void increment(View view) {
         quantity++;
-        displayQuantity(quantity);
+        display(quantity);
     }
 
-
-    public void decrement(View view)  {
-        if (quantity>0) {
+    /**
+     * This method is called when the Decrement/Minus button is clicked.
+     */
+    public void decrement(View view) {
+        if(quantity>0) {
             quantity--;
-            displayQuantity(quantity);
+            display(quantity);
         }
     }
 
-
+    /**
+     * This method is called when the order button is clicked.
+     */
     public void submitOrder(View view) {
-        // Figure out if user wants Whipped Cream
-        EditText nameField = (EditText) findViewById(R.id.name_field)
-        String value = nameField.getText().toString();
-        Log.v(tag: "MainActivity", msg: "Name: " + value);
+        //Get text from the EditText XML
+        EditText nameField = (EditText) findViewById(R.id.user_input_name_view);
+        String name = nameField.getText().toString();
+        Log.v("MainActivity", "Name:"+ name);
 
-        // Figure out if user wants chocolate topping
-        CheckBox chocolateCheckBox = (CheckBox) findViewById(R.id.chocolate_checkbox);
-        boolean hasChocolate = chocolateCheckBox.isChecked();
 
-        // Calculate Price
-        int price = calculatePrice();
-
-        // Display the order summary on the screen
-        CheckBox whippedCreamCheckBox= (CheckBox) findViewById(R.id.whipped_cream_checkbox);
+        //Figure out if user wants whipped cream
+        CheckBox whippedCreamCheckBox = (CheckBox)findViewById(R.id.whipped_cream_checkbox);
         boolean hasWhippedCream = whippedCreamCheckBox.isChecked();
-        Log.v(tag: "MainActivity", msg: "Has whipped cream: " + hasWhippedCream)
+        Log.v("MainActivity", "Has whipped cream:" + hasWhippedCream);
+
+        //Figure out if user wants chocolate topping
+        CheckBox chocolateCheckBox = (CheckBox)findViewById(R.id.chocolate);
+        boolean hasChocolate = chocolateCheckBox.isChecked();
+        Log.v("MainActivity", "Add Chocolate Topping: " + hasChocolate);
+
+
+
+        int price = calculatePrice();
+        String priceMessage = createOrderSummary(price,hasWhippedCream, hasChocolate, name);
+        displayMessage(priceMessage);
+
+
+
+        //Display the order summary on the string
+
+
 
     }
+
+
+
+    int calculatePrice(){
+        int price = quantity * 5;
+        return price;
+
+    }
+    /**
+     * This method displays the given quantity value on the screen.
+     */
+    private void display(int number) {
+        TextView quantityTextView = (TextView) findViewById(R.id.quantity_text_view);
+        quantityTextView.setText("" + number);
+    }
+
+    /**
+     * This method displays the given price on the screen.
+     */
+    private void displayPrice(int number) {
+        TextView priceTextView = (TextView) findViewById(R.id.price_text_view);
+        priceTextView.setText(NumberFormat.getCurrencyInstance().format(number));
+    }
+
     /**
      * This method displays the given text on the screen.
      */
-    private int calculatePrice() {
-        int price = quantity * 5;
-        return price;
-    }
-
-    /**
-     *
-     * @param price of the order
-     * @param addWhippedCream is whether or not the user wants whipped cream topping
-     * @return priceMessage
-     */
-
-    /**
-     * Create a summary of our order
-     *
-     * @param price
-     * @return priceMessage
-     */
-    private String createOrderSummary(int price, boolean addWhippedCream, boolean addChocolate) {
-        String priceMessage = "Thank you for ordering " + quantity + " Coffees! \nAmount Due: $" + price;
-        priceMessage += "\nAdd Whipped Cream? " + addWhippedCream;
-        priceMessage += "/nAdd Chocolate Topping " + addChocolate;
-        priceMessage += "\n\nYour order will be right up!";
-        return priceMessage;
-    }
-
     private void displayMessage(String message) {
         TextView priceTextView = (TextView) findViewById(R.id.price_text_view);
         priceTextView.setText(message);
     }
 
-    private void displayQuantity(int number) {
-        TextView quantityTextView = (TextView) findViewById(R.id.quantity_text_view);
-        quantityTextView.setText(" " + number);
-    }}
+
+
+    /***
+     * Create a summary of our order
+     * @param price
+     * @return priceMessage
+     * @param hasWhippedCream is whether or not the user wants Whipped Cream Topping
+     * @param name of the customer
+     */
+
+
+    private String createOrderSummary(int price, boolean hasWhippedCream, boolean addChocolate, String name) {
+        String priceMessage ="\nName: "+ name;
+        priceMessage += "\nThank you for ordering " + quantity + " Coffees!";  //I used the escape key \n to put info on a new line
+        priceMessage += "\nAdd Whipped Cream? " + hasWhippedCream;
+        priceMessage += "\nAdd Chocolate Topping" + addChocolate;
+        priceMessage += "\nAmount Due: $" + price;
+        priceMessage += "\n\nYour order will be right up!"; //Double \n escape key for w line separation
+        return priceMessage;
+
+
+    }
+
+
+
+
+
+
+
+
+
+}
